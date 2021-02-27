69fe81775e0e28d9e807dc14b8cffa6881f2b75c
BrianChiu180 <71215856+BrianChiu180@users.noreply.github.com>
2021-02-28 01:01:50 +0800

    Update MLX90614_OLED-10.ino

--- MLX90614_OLED-10.ino
+++ MLX90614_OLED-10.ino
@@ -9,9 +9,8 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 #include <HTTPClient.h>
-String Linetoken = "Mij04QbPme4LUUymgW39uQN7QYCTR0gZststgM4T9tr";                            //Your New Line Token
-//String Linetoken = "vg0ZKjA0aThL3tuWUAGG73xaes7WiD9e0yFplu0K5vx";                            //Your Line Token
-//String Linetoken = "jCwIE0nJZD5NA5DAW3AC7xYjABWnALl4GjEkZpin9M3";                          //Your Line Token for group 
+String Linetoken = "Your New Line Token";                                                    //Your New Line Token
+
 
 int threshold = 4;                                                                          //MLX90614 Calibration for Body mode (Surface + 4 C=Body)
 float temp,Objtemp1,Objtemp2; 
@@ -20,19 +19,17 @@ uint16_t result;
 
 #define SCREEN_WIDTH 128               
 #define SCREEN_HEIGHT 32                                                                    //Set OLED pixel=128 * 32  
-#define pin2 26
-#define pin3 27
+#define pin2 26                                                                             // Switch to measure Surface Mode
+#define pin3 27                                                                             // Switch to measure Body Mode
 
-WiFiClientSecure client;                                                                     //網路連線物件
+WiFiClientSecure client;                                                                     //WIFIClient library name use symbol to replace 
 char host[] = "notify-api.line.me";                                                          //LINE Notify API web address
 
 
 // Your WiFi credentials.
 // Set password to "" for open networks.
-char ssid[] = "HITRON-6E00";  //Home WIFI SSID
-char password[] = "0921294757";  //Home WIFI Password
-// char ssid[] = "CPH1607";  //Phone WIFI SSID
-// char password[] = "12345678";  //Phone WIFI Password
+char ssid[] = "WIFI SSID";  //Home WIFI SSID
+char password[] = "WIFI Password";  //Home WIFI Password
 
 String url = "https://api.thingspeak.com/update?api_key=KE393NY4C7E4Y3UH&field1=25";        //Set Thingspeak chanel 
 
@@ -54,16 +51,16 @@ void setup()
   // Initial WIFI
   WiFi.begin(ssid, password);
 
-  while (WiFi.status() != WL_CONNECTED) //Wait for connection ok
+  while (WiFi.status() != WL_CONNECTED)                                                                         //Wait for connection ok
   {
     delay(500);
     Serial.print(".");
   }
 
   Serial.println("");
-  Serial.println("WiFi connected"); //WIFI cooection ok
+  Serial.println("WiFi connected");                                                                            //WIFI cooection ok
   Serial.println("IP address: "); 
-  Serial.println(WiFi.localIP()); //列印模塊IP
+  Serial.println(WiFi.localIP());                                                                              //Disaply ESP32 WIFI IP
 
   // Detect OLED installtion ok or not and Address 0x3C for 128x32
     if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) 
@@ -72,7 +69,7 @@ void setup()
        for(;;); // Don't proceed, loop forever
       }         
   init_display();
-  Serial.println("MLX90614 infra-red temperature sensor test");                            //Initalation IR sensor-MLX90614
+  Serial.println("MLX90614 infra-red temperature sensor test");                                                //Initalation IR sensor-MLX90614
   mlx.begin();                        
 }
  
@@ -119,8 +116,8 @@ void init_display()
 {
   display.clearDisplay();
   display.setRotation(2);                                                                   //OLED顯示轉向，可自行決定方向
-  display.setTextSize(2);                                                                   // 設定文字大小
-  display.setTextColor(1);                                                                  // 1:OLED預設的顏色(這個會依該OLED的顏色來決定)
+  display.setTextSize(2);                                                                   // Set Text size
+  display.setTextColor(1);                                                                  // Set OLD Color (Default is 1)
   display.setCursor(0,0);                                                                  // Start at top-left corner
   display.print("YZU_Brian");
   display.display();
@@ -144,7 +141,7 @@ void get_surface()
     Wire.beginTransmission(0x5A);                                                                          //Start to send MLX90614 I2C address-0x5A
     Wire.write(0x07);                                                                                      // Write I2C data to MLX90614 address-0x07 (Ogject Temperature)
     Wire.endTransmission(false);                                                                           // I2C stop transmission
-    Wire.requestFrom(0x5A, 3);                                                                             //向已知地址slave獲取連續3個數據，這時候需要注意，數據只是存起來了，並沒有真正返回
+    Wire.requestFrom(0x5A, 3);                                                                             //Request the I2C data with 3 Bytes from the address of MLX90614
     result = Wire.read();                                                                                  //Receive DATA
     result |= Wire.read() << 8;                                                                            //Receive DATA
     uint8_t pec = Wire.read();
@@ -161,7 +158,7 @@ void get_body()
     Wire.beginTransmission(0x5A);                                                                          //Start to send MLX90614 I2C address-0x5A
     Wire.write(0x07);                                                                                      // Write I2C data to MLX90614 address-0x07 (Ogject Temperature)
     Wire.endTransmission(false);                                                                           // I2C stop transmission
-    Wire.requestFrom(0x5A, 3);                                                                             //向已知地址slave獲取連續3個數據，這時候需要注意，數據只是存起來了，並沒有真正返回
+    Wire.requestFrom(0x5A, 3);                                                                             //Request the I2C data with 3 Bytes from the address of MLX90614
     result = Wire.read();                                                                                  //Receive DATA
     result |= Wire.read() << 8;                                                                            //Receive DATA
     uint8_t pec = Wire.read();
@@ -178,10 +175,10 @@ void web_surface()
               //Forward its value to Thingspeak
               Serial.println("Connect Web");
               HTTPClient http;
-//   String url1 = url + "&field1=" + (int)mlx.readObjectTempC();                          // Show the value of Temperature and Humidity by parameter with Http get on Web ; 將溫度及濕度以http get參數方式補入網址後方
-              String url1 = url + "&field1=" + (int)Objtemp1;                               // Show the value of Temperature and Humidity by parameter with Http get on Web ; 將溫度及濕度以http get參數方式補入網址後方
-//              String url1 = url + "&field1=" + (float)Objtemp1;                               // Show the value of Temperature and Humidity by parameter with Http get on Web ; 將溫度及濕度以http get參數方式補入網址後方
-              http.begin(url1);                                                                      //Get the content of heep client ; http client取得網頁內容
+//   String url1 = url + "&field1=" + (int)mlx.readObjectTempC();                                         // Show the value of Temperature and Humidity by parameter with Http get on Web 
+              String url1 = url + "&field1=" + (int)Objtemp1;                                             // Show the value of Temperature and Humidity by parameter with Http get on Web 
+//              String url1 = url + "&field1=" + (float)Objtemp1;                                         // Show the value of Temperature and Humidity by parameter with Http get on Web
+              http.begin(url1);                                                                           //Get the content of heep client 
               int httpCode = http.GET();
               if (httpCode == HTTP_CODE_OK)      
                   {
@@ -202,10 +199,8 @@ void web_body()
   //Forward its value to Thingspeak
                Serial.println("Connect Web");
                HTTPClient http;
-//   String url1 = url + "&field1=" + (int)mlx.readObjectTempC();                          // Show the value of Temperature and Humidity by parameter with Http get on Web ; 將溫度及濕度以http get參數方式補入網址後方
-            String url2 = url + "&field1=" + (int)Objtemp2;                               // Show the value of Temperature and Humidity by parameter with Http get on Web ; 將溫度及濕度以http get參數方式補入網址後方
-//              String url2 = url + "&field1=" + (float)Objtemp2;                               // Show the value of Temperature and Humidity by parameter with Http get on Web ; 將溫度及濕度以http get參數方式補入網址後方
-              http.begin(url2);                                                                      //Get the content of heep client ; http client取得網頁內容
+               String url2 = url + "&field1=" + (int)Objtemp2;                                       // Show the value of Temperature and Humidity by parameter with Http get on Web
+              http.begin(url2);                                                                      //Get the content of heep client
               int httpCode = http.GET();
               if (httpCode == HTTP_CODE_OK)      
                   {
@@ -224,36 +219,32 @@ void web_body()
 void Line_surface()
 {
   //ESP32 will send its warming if the temperaure is over than 30C
-//  if (mlx.readObjectTempC() >= 30 ) 
   if (Objtemp1 >= 20 )
   {
     //組成Line訊息內容
-//    String message = "目前待測物溫度有偏高趨勢, 請派人前往處理";                               //The message content of Line
-    String message = "The Temperaute will risk";                               //The message content of Line
-//    message += "\nTemperature=" + String((int)mlx.readObjectTempC()) + " *C";           //The message content of Line
-   message += "\nTemperature=" + String((int)Objtemp1) + " *C";           //The message content of Line
+    String message = "The Temperaute will risk";                                                           //The message content of Line
+    message += "\nTemperature=" + String((int)Objtemp1) + " *C";           //The message content of Line
     
     Serial.println(message);
     if (client.connect(host, 443))
     {
       int LEN = message.length();
-      String url1 = "/api/notify";                                                       //傳遞POST表頭
-//      String url1 = "https://notify-api.line.me/api/notify";
-      client.println("POST " + url1 + " HTTP/1.1");                                      //傳遞POST表頭
-      client.print("Host: "); client.println(host);                                      //傳遞POST表頭
+      String url1 = "/api/notify";                                                       //Pass POST header
+      client.println("POST " + url1 + " HTTP/1.1");                                      //Pass POST header
+      client.print("Host: "); client.println(host);                                      //Pass POST header
       
-      client.print("Authorization: Bearer "); client.println(Linetoken);                //權杖
-      client.println("Content-Type: application/x-www-form-urlencoded");                //權杖
-      client.print("Content-Length: "); client.println( String((LEN + 8)) );            //權杖
-      client.println();                                                                 //權杖
-      client.print("message="); client.println(message);                                //權杖
+      client.print("Authorization: Bearer "); client.println(Linetoken);                //access token
+      client.println("Content-Type: application/x-www-form-urlencoded");                //access token
+      client.print("Content-Length: "); client.println( String((LEN + 8)) );            //access token
+      client.println();                                                                 //access token
+      client.print("message="); client.println(message);                                //access token
       client.println();
       
-      delay(1000);                                                                        //等候1 sec 候回應 
+      delay(1000);                                                                        //Delay 1 sec 
       String response = client.readString();
 
-      Serial.println(response);                                                         //顯示傳遞結果
-      client.stop();                                                                    //斷線，否則只能傳5次
+      Serial.println(response);                                                         //Display send complete
+      client.stop();                                                                    //Stop client connection
     }
     else 
     {
@@ -279,23 +270,22 @@ void Line_body()
     if (client.connect(host, 443)) 
     {
       int LEN = message.length();
-      String url2 = "/api/notify";                                                       //傳遞POST表頭
-      //String url2 = "https://notify-api.line.me/api/notify";
-      client.println("POST " + url2 + " HTTP/1.1");                                      //傳遞POST表頭
-      client.print("Host: "); client.println(host);                                      //傳遞POST表頭
+      String url2 = "/api/notify";                                                       //Pass POST header
+      client.println("POST " + url2 + " HTTP/1.1");                                      //Pass POST header
+      client.print("Host: "); client.println(host);                                      //Pass POST header
       
-      client.print("Authorization: Bearer "); client.println(Linetoken);                //權杖
-      client.println("Content-Type: application/x-www-form-urlencoded");                //權杖
-      client.print("Content-Length: "); client.println( String((LEN + 8)) );            //權杖
-      client.println();                                                                 //權杖
-      client.print("message="); client.println(message);                                //權杖
+      client.print("Authorization: Bearer "); client.println(Linetoken);                //access token
+      client.println("Content-Type: application/x-www-form-urlencoded");                //access token
+      client.print("Content-Length: "); client.println( String((LEN + 8)) );            //access token
+      client.println();                                                                 //access token
+      client.print("message="); client.println(message);                                //access token
       client.println();
       
-      delay(1000);                                                                        //等候1 sec 候回應 
+      delay(1000);                                                                        //Delay 1 sec 
       String response = client.readString();
 
-      Serial.println(response);                                                         //顯示傳遞結果
-      client.stop();                                                                    //斷線，否則只能傳5次
+      Serial.println(response);                                                         //Display send complete
+      client.stop();                                                                    //Stop client connection
     }
     else 
     {
